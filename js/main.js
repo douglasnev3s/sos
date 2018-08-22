55e591a1bc75d49ad7cef79a099e40c1cf0d3ce4
Douglas Neves <douglas.neves@gmail.com>
2018-08-22 09:32:14 -0300

    Inserção do Preloader no site

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+//Preloader
+
+jQuery(document).ready(function ($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).on('load', function () {
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});
+
 //Plugin Modal
 
 // Get the modal
